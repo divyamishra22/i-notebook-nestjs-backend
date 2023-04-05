2c3ab6ba1797b7caf140ca9473904d027b0f77d2
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-04-05 16:40:42 +0530

    modules of user and note created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { NoteController } from './note/note.controller';
+import { NoteService } from './note/note.service';
+import { NoteModule } from './note/note.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb://divya:divya123@127.0.0.1:27017/mydb1',
-    )
+    ),
+    UserModule,
+    NoteModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UserController, NoteController],
+  providers: [AppService, UserService, NoteService],
 })
 export class AppModule {}
