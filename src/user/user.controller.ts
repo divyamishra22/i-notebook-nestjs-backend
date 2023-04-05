4cdbc5f26e9bdf651c765486f0d23305a1f29c20
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-04-05 23:52:30 +0530

    data saved in mongo+data validation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,11 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ApiProperty, ApiTags } from '@nestjs/swagger';
+import { UserService } from './user.service';
+import { User } from './user.schema';
 
 
 class UserCreateRequestBody{
-    @ApiProperty() username: string;
+    @ApiProperty() name: string;
     @ApiProperty() password: string;
     @ApiProperty() email: string;
 }
@@ -12,12 +14,13 @@ class UserCreateRequestBody{
 @ApiTags('Users')
 @Controller('user')
 export class UserController {
+    constructor(private userService: UserService) {}
 
     @Post('/')
 //CreateUser(@Param('username') username: string):string{
    // return `User of ${username} created`;
-async createNewUser(@Body() createUserRequest: UserCreateRequestBody): Promise<any>{         
-  
-  return 'user';                                                     
+ async createNewUser(@Body() createUserRequest: UserCreateRequestBody): Promise<User>{         
+ const  user = await this.userService.createUser(createUserRequest);
+  return user;                                                     
    }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { User } from './user.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+    //create user
+  public   async createUser(user: Partial<User>):Promise<User>{
+    const existinguser = await this.userModel.findOne({where: {email: user.email}})
+           if(!existinguser){
+        return await this.userModel.create(user);
+          } 
+          else{
+            throw new ConflictException('Email already exists');
+          }
+    }
+}
