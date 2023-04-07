fe76c589d69bad78c8aa6873b84e00284865e40f
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-04-08 00:44:32 +0530

    notes schema added

--- src/note/note.module.ts
+++ src/note/note.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
+import { NoteService } from './note.service';
+import { NoteController } from './note.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Note, NoteSchema } from './note.schema';
 
-@Module({})
+@Module({imports: [
+    MongooseModule.forFeature([{name: Note.name, schema: NoteSchema }]),
+    
+  ],
+  controllers: [NoteController],
+  providers: [NoteService],
+  
+})
+
 export class NoteModule {}
--- src/note/note.schema.ts
+++ src/note/note.schema.ts
@@ -1,22 +1,30 @@
 import {Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
+import { ApiProperty } from "@nestjs/swagger";
 // import { Schema } from "mongoose";
-// import { HydratedDocument } from 'mongoose';
+import mongoose, { Document,   } from "mongoose";
 
-// export type UserDocument = HydratedDocument<User>;
 
+export type NoteDocument = Note & Document;
 @Schema()
 export class Note {
+
+   user: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+
+  @ApiProperty()
   @Prop({required:true})
   title: string;
 
+  
   @Prop({required:true , unique:true})
   description: string;
 
+  
   @Prop({required:true})
   tag: string;
 
-  @Prop({ default:Date.now })
-  date = Date;
+  
+  @Prop({ optional: true})
+  date:  Date;
 
 
 }
