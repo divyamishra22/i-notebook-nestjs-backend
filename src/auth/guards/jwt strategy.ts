49715cfc036073e76c2d4c4780dd1119bc261bfe
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-04-09 13:51:07 +0530

    strategy declared

--- src/auth/guards/jwt strategy.ts
+++ src/auth/guards/jwt strategy.ts
@@ -1,11 +1,22 @@
 import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
-import { Strategy } from "passport-jwt";
+import { ExtractJwt, Strategy } from "passport-jwt";
 
 
 
 @Injectable()
 export class JwtStrategy extends  PassportStrategy(Strategy){
-    
+ constructor(){
+      
+    super({
+        JwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        ignoreExpiration: false,
+        secretOrKey: 'divya123',
+    });
+ }
+
+ async validate(payload:any){
+    return {...payload.user};
+ }
     
 }
