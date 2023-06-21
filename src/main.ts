a975d5177129c8c7d15ca664d27e04c098ea972b
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-06-21 21:40:55 +0530

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule); 
+  app.enableCors();
   // const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
