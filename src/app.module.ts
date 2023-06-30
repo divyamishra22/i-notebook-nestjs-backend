ce28804ee1e724aff9ca3d7b09b68c7a079f6d07
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-06-30 14:59:10 +0530

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,13 +9,17 @@ import { NoteController } from './note/note.controller';
 import { NoteService } from './note/note.service';
 import { NoteModule } from './note/note.module';
 import { AuthModule } from './auth/auth.module';
-import { ProdDbModule } from './db.module';
-// import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+// import { ProdDbModule } from './db.module';
+
 
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb://divya:divya123@127.0.0.1:27017/mydb1',
+      // 'mongodb://divya:divya123@127.0.0.1:27017/mydb1',
+      'mongodb+srv://divyaa:divyaa@cluster0.nkr9gtp.mongodb.net/mydb1?retryWrites=true&w=majority'
+   
+     
       
     ),
     UserModule,
@@ -23,6 +27,15 @@ import { ProdDbModule } from './db.module';
     AuthModule,
     // ProdDbModule
   ],
+//   imports:[
+//      MongooseModule.forRootAsync({
+//     imports: [ConfigModule],
+//     useFactory: async (configService: ConfigService) => ({
+//       uri: configService.get<string>('MONGODB_URI'),
+//     }),
+//     inject: [ConfigService],
+//   })
+// ],
   controllers: [AppController],
   providers: [AppService],
 })
