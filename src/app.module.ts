412434009de825688262ba6540b22a428261a426
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-06-30 18:16:06 +0530

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,24 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
-import { NoteController } from './note/note.controller';
-import { NoteService } from './note/note.service';
 import { NoteModule } from './note/note.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+
+
 // import { ProdDbModule } from './db.module';
 
 
+require('dotenv').config();
+
 @Module({
   imports: [
+   
     MongooseModule.forRoot(
       // 'mongodb://divya:divya123@127.0.0.1:27017/mydb1',
-      'mongodb+srv://divyaa:divyaa@cluster0.nkr9gtp.mongodb.net/mydb1?retryWrites=true&w=majority'
-   
+      //  'mongodb+srv://divyaa:divyaa@cluster0.nkr9gtp.mongodb.net/mydb1?retryWrites=true&w=majority'
      
+        process.env.MONGODB_URI,
       
     ),
     UserModule,
@@ -27,15 +28,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     AuthModule,
     // ProdDbModule
   ],
-//   imports:[
-//      MongooseModule.forRootAsync({
-//     imports: [ConfigModule],
-//     useFactory: async (configService: ConfigService) => ({
-//       uri: configService.get<string>('MONGODB_URI'),
-//     }),
-//     inject: [ConfigService],
-//   })
-// ],
+  
   controllers: [AppController],
   providers: [AppService],
 })
