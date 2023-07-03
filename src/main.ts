8c22910c48e4b405d0fb7859ce576d9a4b853219
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-07-03 21:44:30 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule); 
-  app.enableCors();
+  app.enableCors({
+    origin:'http://localhost:5173/',
+  });
   // const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
